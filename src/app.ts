f0a544943269feb49ce96c027d9c206591ada919
Imran Noori <imrannoori1919@gmail.com>
2026-07-17 17:59:25 +0430

    feat: added nodemon

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
+import express from "express";
+
+const app = express();
+
+console.clear();
+
+app.listen(3000, () => {
+  console.log("Sever is app an running on port 3000");
+});
+
 console.log("hello world");
